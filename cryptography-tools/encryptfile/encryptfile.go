e1769caf65e65d14b595f26ecb44fb93bc04700e
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2021-12-10 14:53:22 -0800

    update encryptfile with new coding style

--- cryptography-tools/encryptfile/encryptfile.go
+++ cryptography-tools/encryptfile/encryptfile.go
@@ -6,6 +6,7 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -27,22 +28,21 @@ func checkErr(err error) {
 
 }
 
-func checkVersion(version bool) {
+func setLogLevel(logLevel string) error {
 
-	if version {
-		fmt.Println(toolVersion)
-		os.Exit(0)
-	}
+	// SET LOG LEVEL (trace, info or error) None of which exit
+	log.Trace("Set Log Level")
 
-}
-
-func setLogLevel(debugTrace bool) {
-
-	// SET LOG LEVEL
-	if debugTrace {
+	switch logLevel {
+	case "trace":
 		log.SetLevel(log.TraceLevel)
-	} else {
+	case "info":
 		log.SetLevel(log.InfoLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.ErrorLevel)
+		return errors.New("please use trace, info or error")
 	}
 
 	// SET FORMAT
@@ -52,68 +52,104 @@ func setLogLevel(debugTrace bool) {
 	// SET OUTPUT (DEFAULT stderr)
 	log.SetOutput(os.Stdout)
 
+	return nil
+
 }
 
-func readFile(filename string) []byte {
+func readFile(filename string) ([]byte, error) {
 
+	// READ FILE
 	log.Trace("Read the file ", filename)
+
 	fileData, err := ioutil.ReadFile(filename)
-	checkErr(err)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read file: %w", err)
+	}
 	log.Trace("File Data\n--------------------\n", string(fileData), "\n--------------------\n")
-	return fileData
+
+	return fileData, nil
 
 }
 
-func getParaphrase(paraphraseFile string) string {
+func getParaphrase(paraphraseFile string) (string, error) {
 
 	var paraphrase string
 
+	// GET THE PARAPHRASE
+	log.Trace("Get the paraphrase")
+
+	// IS PARAPHRASE USER INPUT OR A FILE
 	if paraphraseFile != "" {
+
+		// FILE
+		log.Trace("Get the paraphrase from file")
 		fmt.Println("Getting the paraphrase from the file", paraphraseFile)
-		paraphrase = string(readFile(paraphraseFile))
+		fileBytes, err := readFile(paraphraseFile)
+		if err != nil {
+			return "", fmt.Errorf("unable to open paraphrase: %w", err)
+		}
+		paraphrase = string(fileBytes)
+
 	} else {
+
+		// USER INPUT
 		log.Trace("Get the paraphrase from User")
 		fmt.Print("What is your secret paraphrase? ")
 		_, err := fmt.Scan(&paraphrase)
-		checkErr(err)
+		if err != nil {
+			return "", err
+		}
+
 	}
-	return paraphrase
+
+	return paraphrase, nil
 
 }
 
 func getKeyByte(paraphrase string) []byte {
 
+	// HASH THE PARAPHRASE
 	log.Trace("Hash the paraphrase'", paraphrase, "'to get 32 byte key")
+
+	// HASH USING MD5 HASH
 	hasher := md5.New()
 	hasher.Write([]byte(paraphrase))
 	hash := hex.EncodeToString(hasher.Sum(nil))
-	log.Trace("Hash is ", hash)
+	log.Info("Hashed paraphrase is ", hash)
 
+	// MAKE KEYBYTE
 	keyByte := []byte(hash)
-	log.Trace("keyByte is ", keyByte)
+	log.Info("Keybyte is ", keyByte)
+
 	return keyByte
 
 }
 
-func encryptFileData(keyByte []byte, plainTextByte []byte) string {
+func encryptFileData(keyByte []byte, plainTextByte []byte) (string, error) {
 
 	// ENCRYPT DATA WITH 32 BYTE KEY AND RETURN CIPHERTEXT
 	log.Trace("Encrypt file with key")
 
 	// GET CIPHER BLOCK USING KEY
 	block, err := aes.NewCipher(keyByte)
-	checkErr(err)
+	if err != nil {
+		return "", fmt.Errorf("unable to get cipher block using keyByte: %w", err)
+	}
 
 	// GET GCM INSTANCE THAT USES THE AES CIPHER
 	gcm, err := cipher.NewGCM(block)
-	checkErr(err)
+	if err != nil {
+		return "", fmt.Errorf("unable to get gcm instance that uses the aes cipher: %w", err)
+	}
 
-	// CREATE A NONCE
+	// CREATE A NONCE AND POPULATE
 	nonce := make([]byte, gcm.NonceSize())
 	// Populates our nonce with a cryptographically secure random sequence
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return "", fmt.Errorf("unable to populates our nonce with a random sequence: %w", err)
 	}
+	log.Info("Nonce is ", nonce)
 
 	// ENCRYPT DATA
 	// Note how we put the Nonce in the beginging,
@@ -123,29 +159,39 @@ func encryptFileData(keyByte []byte, plainTextByte []byte) string {
 	// RETURN HEX
 	cipherText := hex.EncodeToString(cipherTextByte)
 	log.Trace("Encrypted Data\n--------------------\n", cipherText, "\n--------------------\n")
-	return cipherText
+
+	return cipherText, nil
 
 }
 
-func writeCipherText(cipherText string, filename string) {
+func writeCipherText(cipherText string, filename string) error {
 
-	// Write cipherTex TO A FILE
+	// WRITE cipherTex TO A FILE
 	log.Trace("Write cipherText to a file")
 
-	// Create file
+	// CREATE FILE
 	outputFile, err := os.Create(filename)
-	checkErr(err)
+	if err != nil {
+		return fmt.Errorf("unable to create file: %w", err)
+	}
 	defer outputFile.Close()
 
+	// ADD HEADER TO TOP OF FILE
 	_, err = outputFile.WriteString("\nThis secret file was created by Jeff DeCola\n")
-	checkErr(err)
+	if err != nil {
+		return fmt.Errorf("unable to add text to file: %w", err)
+	}
 	t := time.Now()
 	_, err = outputFile.WriteString(t.Format(time.ANSIC) + "\n")
-	checkErr(err)
+	if err != nil {
+		return fmt.Errorf("unable to add text to file: %w", err)
+	}
 	_, err = outputFile.WriteString("\n--------------------------------------------------------------------------------\n")
-	checkErr(err)
+	if err != nil {
+		return fmt.Errorf("unable to add text to file: %w", err)
+	}
 
-	// Chop up the cipherText into lines of 80 characters into a slice.
+	// CHOP UP cipherText INTO LINE OF 80 CHARACTERS INTO A SLICE.
 	a := []rune(cipherText)
 	line := ""
 	numberLines := 0
@@ -156,7 +202,9 @@ func writeCipherText(cipherText string, filename string) {
 		if i > 0 && (i+1)%80 == 0 {
 			line = line + "\n"
 			_, err = outputFile.WriteString(line)
-			checkErr(err)
+			if err != nil {
+				return fmt.Errorf("unable to write to file: %w", err)
+			}
 			// Reset line
 			line = ""
 			numberLines++
@@ -169,15 +217,22 @@ func writeCipherText(cipherText string, filename string) {
 		numberLines++
 		line = line + "\n"
 		_, err = outputFile.WriteString(line)
-		checkErr(err)
+		if err != nil {
+			return fmt.Errorf("unable to write to file: %w", err)
+		}
 	}
 
-	log.Trace("There were ", numberCharacters, " characters and ", numberLines, " lines created")
+	log.Info("There were ", numberCharacters, " characters and ", numberLines, " lines created")
 
+	// ADD FOOTER TO BOTTOM OF FILE
 	_, err = outputFile.WriteString("--------------------------------------------------------------------------------\n\n")
-	checkErr(err)
+	if err != nil {
+		return fmt.Errorf("unable to write to file: %w", err)
+	}
 
-	log.Trace("Wrote output file\n\n")
+	log.Trace("Wrote cipherText to file\n\n")
+
+	return nil
 
 }
 
@@ -185,20 +240,27 @@ func main() {
 
 	// FLAGS
 	versionPtr := flag.Bool("v", false, "prints current version")
-	debugTracePtr := flag.Bool("debug", false, "log trace level")
+	logLevelPtr := flag.String("loglevel", "error", "log level (trace, info or error)")
 	inputFilenamePtr := flag.String("i", "INPUT", "input file")
 	outputFilenamePtr := flag.String("o", "OUTPUT", "output file")
 	paraphraseFilePtr := flag.String("paraphrasefile", "", "use a file as a paraphrase")
 	flag.Parse()
 
 	// CHECK VERSION
-	checkVersion(*versionPtr)
+	if *versionPtr {
+		fmt.Println(toolVersion)
+		os.Exit(1)
+	}
 
-	// SET LOG LEVEL
-	setLogLevel(*debugTracePtr)
+	// SET LOG LEVEL (trace, info or error) None of which exit
+	err := setLogLevel(*logLevelPtr)
+	if err != nil {
+		log.Errorf("Error getting logLevel: %s", err)
+	}
 
+	// PRINT OUT FOR TRACE LOG
 	log.Trace("Version flag = ", *versionPtr)
-	log.Trace("Debug flag = ", *debugTracePtr)
+	log.Trace("Log Level = ", *logLevelPtr)
 	log.Trace("Input Filename = ", *inputFilenamePtr)
 	log.Trace("Output Filename = ", *outputFilenamePtr)
 	log.Trace("Paraphrase File = ", *paraphraseFilePtr)
@@ -206,19 +268,31 @@ func main() {
 	fmt.Println(" ")
 
 	// GET DATA TO ENCRYPT - Read the file - Will be a slice of bytes
-	fileDataToEncrypt := readFile(*inputFilenamePtr)
+	fileDataToEncrypt, err := readFile(*inputFilenamePtr)
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err)
+	}
 
 	// GET PARAPHRASE - Ask the user or use a file
-	paraphrase := getParaphrase(*paraphraseFilePtr)
+	paraphrase, err := getParaphrase(*paraphraseFilePtr)
+	if err != nil {
+		log.Fatalf("Error getting paraphrase: %s", err)
+	}
 
 	// GET KEY BYTE - Hash the paraphrase to get 32 Byte Key
 	keyByte := getKeyByte(paraphrase)
 
 	// ENCRYPT FILE DATA BASED ON PARAPHRASE TO GET cipherText
-	cipherText := encryptFileData(keyByte, fileDataToEncrypt)
+	cipherText, err := encryptFileData(keyByte, fileDataToEncrypt)
+	if err != nil {
+		log.Fatalf("Error getting cipherText: %s", err)
+	}
 
 	// WRITE cipherText TO FILE
-	writeCipherText(cipherText, *outputFilenamePtr)
+	err = writeCipherText(cipherText, *outputFilenamePtr)
+	if err != nil {
+		log.Fatalf("Error writing cipherText to file: %s", err)
+	}
 
 	fmt.Println(" ")
 
